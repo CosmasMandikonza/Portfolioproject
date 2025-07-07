4e1fdc3599db58ef97e5c7ec41e407b5af777273
CosmasMandikonza <cosmas.t.mandikonza@gmail.com>
2025-07-07 17:38:58 -0500

    Update app.py
    Make Flask app listen on all interfaces

--- app.py
+++ app.py
@@ -212,4 +212,4 @@ def internal_error(error):
 if __name__ == '__main__':
     # Ensure the app only runs in debug mode during development
     debug_mode = os.environ.get('FLASK_ENV') == 'development'
-    app.run(debug=debug_mode, host='127.0.0.1', port=5000)
+    app.run(debug=debug_mode, host='0.0.0.0', port=5000)
