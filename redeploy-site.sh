46d40eeb2ecc7f3461eac29b5aeb1ee2962ae936
CosmasMandikonza <cosmas.t.mandikonza@gmail.com>
2025-08-04 18:16:27 +0000

    Updates

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Go to your project directory
 cd ~/Portfolioproject || { echo "Directory not found!"; exit 1; }
 
-# Update your git repo to latest main branch
 git fetch origin main && git reset --hard origin/main
 
-#Spinning any existing containers down to prevent memory issues on vps instances
 docker compose -f docker-compose.prod.yml down
 
-#The spin up  new container instances 
-
-docker compose -f docker-compose.prod.yml -d --build 
+docker compose -f docker-compose.prod.yml up -d --build
 
 echo "Redeploy complete. Portfolio service has been restarted."
 echo "Check status with: docker ps"
+
